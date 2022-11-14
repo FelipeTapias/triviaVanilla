0390b733bd19320a55cfbc4b6490d3151a315be3
FelipeTapias <tapiastuberquiados@gmail.com>
2022-11-14 01:32:28 -0500

    Show result answer was created and spinner for the loading too

--- logic.js
+++ logic.js
@@ -1,15 +1,28 @@
-var step = 1;
 var questions = ['QuestionOne', 'QuestionTwo', 'QuestionThree'];
+var resultAnswer = ['result-good', 'result-wrong']
 
-hiddenQuestion(0);
+initialGame();
+
+function initialGame() {
+    hiddenQuestion(0);
+    hiddenAllResultAnswer();
+    showSpinner(false);
+}
 
 function answeredQuestion(step, answer) {
     console.log(step, answer);
-    hiddenQuestion(step);
+    setTimeout(() => {
+        hiddenQuestion(step);
+        hiddenAllResultAnswer();
+    }, 1000);
+    showSpinner(true);
+    setTimeout(() => {
+        showSpinner(false);
+        showResultAnswer(answer);
+    }, 500);
 }
 
 function hiddenQuestion(question) {
-    
     for(var i = 0; i < questions.length; i++) {
         if(i != question) {
             document.getElementById(questions[i]).style.display = 'none';
@@ -17,4 +30,26 @@ function hiddenQuestion(question) {
     }
     document.getElementById(questions[question]).style.display = 'block';
     
+}
+
+function hiddenAllResultAnswer() {
+    resultAnswer.forEach(element => {
+        document.getElementById(element).style.display = 'none';
+    });
+}
+
+function showResultAnswer(answer) {
+    if(answer) {
+        document.getElementById('result-good').style.display = 'block';
+    }else {
+        document.getElementById('result-wrong').style.display = 'block';
+    }
+}
+
+function showSpinner(show) {
+    if(show){
+        document.getElementById('spinner').style.display = 'block';
+    }else {
+        document.getElementById('spinner').style.display = 'none';
+    }
 }
